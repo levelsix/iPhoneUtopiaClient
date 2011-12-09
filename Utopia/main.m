726aa1ba1f2507289cbc6efd70141b7619e38a7b
Ashwin Kamath <ashwin_kamath@apple.com>
2011-12-09 05:11:39 -0800

    Initial Commit

---
+++ Utopia/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Utopia
+//
+//  Created by Ashwin Kamath on 12/9/11.
+//  Copyright LVL6 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
