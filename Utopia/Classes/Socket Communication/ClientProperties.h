f5f03bba4a6ac9725aae33aaba789255f993fafd
Ashwin Kamath <ashwin@lvl6.com>
2012-05-22 14:32:49 -0700

    added client properties

---
+++ Utopia/Classes/Socket Communication/ClientProperties.h
@@ -0,0 +1,29 @@
+//
+//  ClientProperties.h
+//  Utopia
+//
+//  Created by Ashwin Kamath on 5/22/12.
+//  Copyright (c) 2012 LVL6. All rights reserved.
+//
+
+#ifndef DEBUG
+
+#define HOST_NAME @"184.169.148.243"
+#define HOST_PORT 8888
+
+#define UDID [[UIDevice currentDevice] uniqueDeviceIdentifier]
+#else
+
+#define HOST_NAME @"10.1.10.17"//@"184.169.148.243"
+#define HOST_PORT 8888
+
+#define UDID @"42d1cadaa64dbf3c3e8133e652a2df06"//[[UIDevice currentDevice] uniqueDeviceIdentifier]//@"m";//@"42d1cadaa64dbf3c3e8133e652a2df06"//
+//#define FORCE_TUTORIAL
+#endif
+
+// Tags for keeping state
+#define READING_HEADER_TAG -1
+#define HEADER_SIZE 12
+
+#define RECONNECT_TIMEOUT 0.5f
+#define NUM_SILENT_RECONNECTS 5
--- Utopia/Classes/Socket Communication/SocketCommunication.m
+++ Utopia/Classes/Socket Communication/SocketCommunication.m
@@ -14,28 +14,7 @@
 #import "OutgoingEventController.h"
 #import "Globals.h"
 #import "Apsalar.h"
-
-#ifndef DEBUG
-
-#define HOST_NAME @"184.169.148.243"
-#define HOST_PORT 8888
-
-#define UDID [[UIDevice currentDevice] uniqueDeviceIdentifier]
-#else
-
-#define HOST_NAME @"10.1.10.17"//@"184.169.148.243"
-#define HOST_PORT 8888
-
-#define UDID @"42d1cadaa64dbf3c3e8133e652a2df06"//[[UIDevice currentDevice] uniqueDeviceIdentifier]//@"m";//@"42d1cadaa64dbf3c3e8133e652a2df06"//
-//#define FORCE_TUTORIAL
-#endif
-
-// Tags for keeping state
-#define READING_HEADER_TAG -1
-#define HEADER_SIZE 12
-
-#define RECONNECT_TIMEOUT 0.5f
-#define NUM_SILENT_RECONNECTS 5
+#import "ClientProperties.h"
 
 @implementation SocketCommunication
 
