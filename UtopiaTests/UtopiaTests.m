b9dae943f6583c80b0125397e7e4e1fb54489a00
Kevin Calloway <kevin@lvl6.com>
2012-06-05 18:38:38 -0700

    Added an UtopiaTests target to the Utopia project

---
+++ UtopiaTests/UtopiaTests.m
@@ -0,0 +1,32 @@
+//
+//  UtopiaTests.m
+//  UtopiaTests
+//
+//  Created by Kevin Calloway on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UtopiaTests.h"
+
+@implementation UtopiaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UtopiaTests");
+}
+
+@end
