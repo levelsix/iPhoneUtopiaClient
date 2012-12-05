2be5a7966bd77a86eb60d20f51b52645a9983f27
Ashwin Kamath <ashwin@lvl6.com>
2012-12-05 15:53:28 -0800

    working on towers

---
+++ Utopia/Classes/Game Menus/ClanMenu/ClanTowerTab.h
@@ -0,0 +1,13 @@
+//
+//  ClanTowerTab.h
+//  Utopia
+//
+//  Created by Ashwin Kamath on 12/5/12.
+//  Copyright (c) 2012 LVL6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClanTowerTab : UIView
+
+@end
---
+++ Utopia/Classes/Game Menus/ClanMenu/ClanTowerTab.m
@@ -0,0 +1,31 @@
+//
+//  ClanTowerTab.m
+//  Utopia
+//
+//  Created by Ashwin Kamath on 12/5/12.
+//  Copyright (c) 2012 LVL6. All rights reserved.
+//
+
+#import "ClanTowerTab.h"
+
+@implementation ClanTowerTab
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
